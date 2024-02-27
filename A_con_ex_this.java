ea4c25ca686c06e97daca5facff6c68b1bfb31cd
Shubham Kachare <Kacharerohan21@gmail.com>
2024-02-27 14:07:21 +0530

    first commit

---
+++ A_con_ex_this.java
@@ -0,0 +1,14 @@
+package JavaProject;
+
+public class A_con_ex_this {
+	void show()
+	{
+		System.out.print(this);
+	}
+
+	public static void main(String[] args) {
+		A_con_ex_this r=new A_con_ex_this();
+		r.show();
+	}
+
+}
